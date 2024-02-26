ced2b4379d1eda1a4b8b6e8d8d85c244a5e1d2e6
Dima <dimasalabay0711@gmail.com>
2024-02-26 17:56:36 +0200

    done

--- src/4.ts
+++ src/4.ts
@@ -1,4 +1,76 @@
+/*
+	У цьому завданняi вам належить реалізувати сценарій життя, де людина, ключ і будинок взаємодіють один з одним.
+
+	Ключ (Key): Створіть клас Key. У нього має бути одна приватна властивість signature, яка генерується випадково при створенні об'єкта цього класу (наприклад Math.random()). Також цей клас повинен мати метод getSignature, який повертає значення властивості signature.
+
+	Людина (Person): Створіть клас Person. Конструктор цього класу приймає об'єкт класу Key і зберігає їх у приватному властивості key. Клас Person повинен мати метод getKey, який повертає збережений ключ.
+
+	Дім (House): Створіть абстрактний клас House. Цей клас має дві властивості: door, яка може бути відкрита (true), або закрита (false), і key, яка зберігає об'єкт класу Key. У цьому класі також повинен бути метод comeIn, який додає об'єкт класу Person у масив tenants, якщо door відкрита. Ваш абстрактний клас House також повинен мати абстрактний метод OpenDoor, який приймає об'єкт класу Key.
+
+	Мій будинок (MyHouse): Створіть клас MyHouse, який успадковується від абстрактного класу House. Реалізуйте метод openDoor у цьому класі. Якщо ключ, переданий цьому методу, збігається з ключем, збереженим як key, то двері відчиняються.
+
+	Після реалізації всіх класів створіть об'єкти для кожного класу та спробуйте відтворити сценарій, в якому людина приходить додому.
+*/
+
+abstract class House {
+	protected door: boolean = false;
+	protected key: Key;
+	protected tenants: Person[] = [];
+
+	constructor(key: Key) {
+		this.key = key;
+	}
+
+	abstract openDoor(key: Key): void;
+
+	comeIn(person: Person): void {
+		if (this.door) {
+			this.tenants.push(person);
+			console.log("Person entered the house.");
+		} else {
+			console.log("The door is closed. Person cannot enter.");
+		}
+	}
+}
+
+class MyHouse extends House {
+	openDoor(key: Key) {
+		if (key.getSignature() === this.key.getSignature()) {
+			this.door = true;
+			console.log("The door is now open.");
+		} else {
+			console.log("Invalid key. Can't open the door.");
+		}
+	}
+}
+
+class Key {
+	private signature: number;
+	constructor() {
+		// Випадкове генерування signature
+		this.signature = Math.random();
+	}
+
+	getSignature() {
+		return this.signature;
+	}
+}
+
+class Person {
+	private key: Key;
+
+	constructor(key: Key) {
+		this.key = key;
+	}
+
+	getKey() {
+		return this.key;
+	}
+}
+
 const key = new Key();
+
+console.log(key);
 
 const house = new MyHouse(key);
 const person = new Person(key);
@@ -6,6 +78,3 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
-
-export {};
